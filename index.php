211d5481ba2b89baf1a1d5f913849c9152e0a5fa
sharif Habiman <hdidiersharif@gmail.com>
2023-07-19 17:40:11 +0200

    more changes

--- index.php
+++ index.php
@@ -9,3 +9,12 @@ $mainControllerNameSpace ="OnlineshopOop\\Controllers\\MainController";
 $CartControllerNameSpace ="OnlineshopOop\\Controllers\\CartController";
 $ProductsControllerNameSpace="OnlineshopOop\\Controllers\\ProductsController";
 $UsersControllerNameSpace="OnlineshopOop\\Controllers\\UsersController";
+
+Router::add('/','get',$mainControllerNameSpace,'index');
+Router::add('/login','get', $UsersControllerNameSpace,'login');
+Router::add('/logout','get',$UsersControllerNameSpace,'logout');
+Router::add('/register','get',$UsersControllerNameSpace,'index');
+Router::add('/register','post',$UsersControllerNameSpace,'add');
+Router::add('/cart','get',$CartControllerNameSpace,'add');
+
+Router::run();
